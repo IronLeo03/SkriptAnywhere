6f3ebda1f753d8784796c1aef7434e1680a2e2c2
Leonardo <IronLeo03>
2024-03-15 11:52:18 +0100

    initial commit

---
+++ src/main/java/me/ironleo03/skriptanywhere/SkriptAnywhere.java
@@ -0,0 +1,15 @@
+package me.ironleo03.skriptanywhere;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkriptAnywhere extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
