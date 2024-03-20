6f0e3104dfe397426b554facbc9ca3532335a67e
IronLeo03 <ironleo03@gmail.com>
2024-03-20 20:29:54 +0100

    make SkriptAnywhere singleton

--- src/main/java/me/ironleo03/skriptanywhere/SkriptAnywhere.java
+++ src/main/java/me/ironleo03/skriptanywhere/SkriptAnywhere.java
@@ -2,6 +2,7 @@ package me.ironleo03.skriptanywhere;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.SkriptAddon;
+import lombok.Getter;
 import me.ironleo03.skriptanywhere.elements.ClassInfos;
 import me.ironleo03.skriptanywhere.network.TickableNetworkManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,6 +10,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.IOException;
 
 public class SkriptAnywhere extends JavaPlugin {
+    /**
+     * Instance of SkriptAnywhere
+     * This singleton is required to access the plugin instance from the classes for effects, events and expressions (thank you Skript).
+     */
+    @Getter
+    private static SkriptAnywhere instance;
     private TickableNetworkManager tickableNetworkManager;
     private SkriptAddon skriptAddon;
 
@@ -20,6 +27,7 @@ public class SkriptAnywhere extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Starting SkriptAnywhere");
+        instance = this;
         getLogger().info("Now starting TickableNetworkManager");
         try {
             tickableNetworkManager = new TickableNetworkManager();
