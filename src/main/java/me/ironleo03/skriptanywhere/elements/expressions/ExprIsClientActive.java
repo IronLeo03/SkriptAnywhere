0726a212bbb3670a0564801c965022c62566591b
IronLeo03 <ironleo03@gmail.com>
2024-03-21 19:50:22 +0100

    add expression to check client connection

---
+++ src/main/java/me/ironleo03/skriptanywhere/elements/expressions/ExprIsClientActive.java
@@ -0,0 +1,25 @@
+package me.ironleo03.skriptanywhere.elements.expressions;
+
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import me.ironleo03.skriptanywhere.network.client.AnywhereSocket;
+
+public class ExprIsClientActive extends SimplePropertyExpression<AnywhereSocket, Boolean> {
+    static {
+        register(ExprIsClientActive.class, Boolean.class, "active", "anywheresocket");
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "message";
+    }
+
+    @Override
+    public Boolean convert(AnywhereSocket anywhereSocket) {
+        return anywhereSocket.getSocketChannel().isConnected();
+    }
+
+    @Override
+    public Class<? extends Boolean> getReturnType() {
+        return Boolean.class;
+    }
+}
